64aa1dcbbf87492fe2fcdb37d3076efe3f9ebbaa
ShafiaChy <shafiachy04@gmail.com>
2023-04-05 21:51:41 +0600

    payment, pagination, user db

--- index.js
+++ index.js
@@ -27,47 +27,79 @@ async function run() {
   try {
     const database = client.db("bistro-boss");
     const paymentsCollection = database.collection("payment");
-    const userCollection = database.collection("users");
+    const usersCollection = database.collection("users");
     const itemsCollection = database.collection("items");
     const cartsCollection = database.collection("carts");
 
-    // GET API
+    //GET USERS API
+    app.get("/users", async (req, res) => {
+      const query = {};
+      const users = await usersCollection.find(query).toArray();
+      res.send(users);
+    });
+    //POST USERS API
+    app.post("/users", async (req, res) => {
+      const user = req.body;
+      console.log(user);
+      const result = await usersCollection.insertOne(user);
+      res.send(result);
+    });
+
+    // GET MENU ITEMS
     app.get("/items", async (req, res) => {
       const cursor = itemsCollection.find({});
       const products = await cursor.toArray();
       res.send(products);
     });
 
-    // POST API
+    //GET SHOP ITEMS
+    app.get("/shopitem", async (req, res) => {
+      const page = parseInt(req.query.page);
+      const size = parseInt(req.query.size);
+      const category = req.query.category;
+      const query = { category: category };
+
+      const cursor = itemsCollection.find(query);
+      const products = await cursor
+        .skip(page * size)
+        .limit(size)
+        .toArray();
+
+      const count = await itemsCollection.countDocuments(query);
+      res.send({ count, products });
+    });
+
+    // POST ITEMS API
     app.post("/addItems", async (req, res) => {
       const product = req.body;
       const result = await itemsCollection.insertOne(product);
       res.json(result);
     });
 
-    app.get("/users", async (req, res) => {
-      const cursor = userCollection.find({});
-      const users = await cursor.toArray();
-      res.send(users);
-    });
+    //GET A USER'S CART
     app.get("/carts", async (req, res) => {
-      const cursor = cartsCollection.find({});
-      const carts = await cursor.toArray();
-      res.send(carts);
+      const email = req.query.email;
+      const cartQuery = { email: email };
+      const cart = await cartsCollection.find(cartQuery).toArray();
+
+      res.send(cart);
     });
 
+    //POST A USER'S CART
     app.post("/carts", async (req, res) => {
       const carts = req.body;
       console.log(carts);
       // TODO: make sure you do not enter duplicate user email
       // only insert users if the user doesn't exist in the database
       const result = await cartsCollection.insertOne(carts);
+      console.log(result);
       res.send(result);
     });
 
     app.post("/create-payment-intent", async (req, res) => {
       const order = req.body;
-      const price = order.price;
+      console.log(order.total);
+      const price = parseFloat(order.total);
       const amount = price * 100;
 
       const paymentIntent = await stripe.paymentIntents.create({
@@ -81,23 +113,45 @@ async function run() {
       });
     });
 
-    // app.post("/payments", async (req, res) => {
-    //   const payment = req.body;
-    //   const result = await paymentsCollection.insertOne(payment);
-    //   const id = payment.orderId;
-    //   const filter = { _id: ObjectId(id) };
-    //   const updatedDoc = {
-    //     $set: {
-    //       paid: true,
-    //       transactionId: payment.transactionId,
-    //     },
-    //   };
-    //   const updatedResult = await bookingsCollection.updateOne(
-    //     filter,
-    //     updatedDoc
-    //   );
-    //   res.send(result);
+    app.delete("/carts/:email", async (req, res) => {
+      const email = req.params.email;
+      const filter = { email: email };
+      const result = await cartsCollection.deleteOne(filter);
+      res.send(result);
+    });
+
+    app.get("/payments", async (req, res) => {
+      const email = req.query.email;
+      const paymentQuery = { email: email };
+      const payment = await paymentsCollection.find(paymentQuery).toArray();
+
+      res.send(payment);
+    });
+
+    app.post("/payments", async (req, res) => {
+      const payment = req.body;
+      const result = await paymentsCollection.insertOne(payment);
+
+      res.send(result);
+    });
+
+    //   app.get('/jwt', async (req, res) => {
+    //     const email = req.query.email;
+    //     const query = { email: email };
+    //     const user = await usersCollection.findOne(query);
+    //     if (user) {
+    //         const token = jwt.sign({ email }, process.env.ACCESS_TOKEN, { expiresIn: '1h' })
+    //         return res.send({ accessToken: token });
+    //     }
+    //     res.status(403).send({ accessToken: '' })
     // });
+
+    //   app.get('/users/admin/:email', async (req, res) => {
+    //     const email = req.params.email;
+    //     const query = { email }
+    //     const user = await usersCollection.findOne(query);
+    //     res.send({ isAdmin: user?.role === 'admin' });
+    // })
   } finally {
   }
 }
